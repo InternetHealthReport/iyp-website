8a8107aa86a31d7b00e0f719c62faaa652c57faf
AayushmanChulet <137825654+AayushmanChulet@users.noreply.github.com>
2025-04-02 08:29:31 +0530

    Fix anchor tags and add toast notification (#6)
    Fix anchor tags for the FAQ and Contact Us sections. Add a toast notification element that triggers whenever a query is copied.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,19 +1,14 @@
 function toClipboard(elem) {
   // Get the text field and button
   var copyText = document.getElementById(elem);
-  var btn = document.getElementById(elem + '-btn');
-  
-  // Store the original button text
-  var originalText = btn.innerText;
 
   // Copy the text inside the text field
   navigator.clipboard.writeText(copyText.innerText);
   
-  // Change button text to "Query copied!"
-  btn.innerText = "Query copied!";
+  // Get the toast notification element
+  const toastElement = document.getElementById("toastElement");
 
-  // Reset button text after 3 seconds
-  setTimeout(() => {
-    btn.innerText = originalText;
-  }, 3000);
+  // Show the toast notification
+  const toast = bootstrap.Toast.getOrCreateInstance(toastElement);
+  toast.show();
 }
